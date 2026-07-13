0215a8e589b8f0363235d2cb13f62d34831a7811
alexandergerhert <oleksandr.herhert.25@kse.org.ua>
2026-07-13 17:40:59 +0300

    Прочитано json файл та змінено NA значення на 0

--- Assignment 4 H_Oleksandr.py
+++ Assignment 4 H_Oleksandr.py
@@ -1,14 +1,24 @@
 import csv
 import json
 with open("global_sales.csv","r",encoding="utf-8") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
+    global_sales = csv.DictReader(file)
+    for row in global_sales:
         try:
             row["quantity"] = float(row["quantity"])
             row["revenue"] = float(row["revenue"])
         except ValueError:
             row["quantity"] = 0
             row["revenue"] = 0
+with open("regional_tariffs.json","r", encoding="utf-8") as json_file:
+    regional_tariffs = json.load(json_file)
+    for region, tariff in regional_tariffs.items():
+        try:
+            tariff = float(tariff)
+        except ValueError:
+            tariff = 0
+
+
+
 
 
 
